f2f4f86f7991fbb1c0a77fa52f6842fda6f5bd1e
Arpitha <arpitha@192.168.0.34>
2019-02-21 15:40:24 -0500

    Fibonacci Series

--- LearningJava/src/com/learning/Fibonacci.java
+++ LearningJava/src/com/learning/Fibonacci.java
@@ -1,14 +1,14 @@
 package com.learning;
 
-public class Fibonacci {  // Fibonacci series
+public class Fibonacci {  // Its a series where next term is the sum of previous 2 terms
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       int n=10, a=0, b=1;
+       int n=10, a=0, b=1;     //n= 10, checking for first 10 values and started with 0 and 1
        System.out.println("First +n terms");
        for(int i=1; i<=n; ++i)
        {
-    	   System.out.println(a+ "+");
+    	   System.out.println(a+ ",");
                int sum = a+b;
                a = b;
                b = sum;
