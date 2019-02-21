0ed5e531d627b8e17b6b551b790ccdf61a680391
Sridhar, Ravishankar <ravishankar.sridhar@rbc.com>
2019-02-21 15:19:53 -0500

    Added new code for Simple Add and modified HelloWorld code

--- LearningJava/src/com/learning/HelloWorld.java
+++ LearningJava/src/com/learning/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello World!");
+		System.out.println("You are doing a good job leaning!");
 		
 	}
 
---
+++ LearningJava/src/com/learning/SimpleAdd.java
@@ -0,0 +1,17 @@
+package com.learning;
+
+public class SimpleAdd {
+
+	static int a = 10;
+	static int b = 20;
+	
+	static int sum;
+	
+	public static void main(String[] args) {
+		
+		sum = a + b;
+		System.out.println("The sum of two numbers:" + sum);
+		
+	}
+
+}
